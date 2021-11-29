ae8f44bd67dcbdbd5887902a2eec0f43b631ad0d
Alejandro reyes <agreyesaldeco@gmail.com>
2021-11-29 14:09:27 -0800

    added Acme assignment

---
+++ Acme/Acme/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \n");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
