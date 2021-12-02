b20ed983cb39600ac765c150532f3a03b8a9d400
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-02 15:10:06 -0800

    MAIN METHOD SUBMISSION ASSIGNMENT

---
+++ MathSubmission/MathSubmission/Oper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSubmission
+{
+    public class Oper
+    {
+        public int Operation1(int a)                    // creates a method that recives an integer data type
+        {
+            return a * a;                               // perfoms a multiplication operation
+        }
+
+        public int Operation1(decimal a)                // overloads a method and recives a decimal data type
+        {
+            return (int)a % 6;                          // performs a modulus operation
+        }
+
+        public int Operation1(string a)                 // overloads a method and recives astring data type
+        {
+            return Int32.Parse(a) + 77;                 // performs an addition
+        }
+    }
+}
---
+++ MathSubmission/MathSubmission/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// THIS EXAMPLE SHOWS HOW IS OVERLOADING IN METHODS
+
+namespace MathSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            Oper a = new Oper();                                                            // DECLARATION AND INSTANTIATION OF A CLASS
+
+            Console.WriteLine("Please, Write a Integer Number : ");
+
+            int num = Convert.ToInt32(Console.ReadLine());                                  // CAST A STRING AS A INTEGER DATA TYPE
+
+            Console.WriteLine(" {0} x {0} is equals to  {1}", num, a.Operation1(num)) ;     // PASS num VARIABLE AS A PARAMETER
+
+
+            Console.WriteLine("Please, Write a Float Number : ");
+
+            decimal num2 = decimal.Parse(Console.ReadLine());                               // CAST A STRING AS A DECIMAL DATA TYPE
+
+            Console.WriteLine(" {0} % 6 is equals to  {1}", num2, a.Operation1(num2));      // PASS num2 VARIABLE AS A PARAMETER
+
+
+
+            Console.WriteLine("Please, Write another integer Number : ");
+            string num3 = Console.ReadLine();                                               // REQUESTING A STRING
+
+            Console.WriteLine(" {0} + 77 is equals to  {1}", num3, a.Operation1(num3));     // PASS num3 VARIABLE AS A PARAMETER
+
+            Console.Read();
+        }
+    }
+}
