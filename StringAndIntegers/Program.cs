efa265b1444f0c4dbe343a1a6f414f0bc820936c
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-02 10:24:50 -0800

    CONSOLE APP STRINGS AND INTEGERS SUBMISSION ASSIGNMENT

--- StringAndIntegers/Program.cs
+++ StringAndIntegers/Program.cs
@@ -6,25 +6,35 @@ class Program
 {
     static void Main()
     {
-        List<int> numbers = new List<int>();
+        List<int> numbers = new List<int>();                                                // DECLARATION OF A INTEGER LIST AND ADDING SIX ELEMENTS
         numbers.Add(45);
         numbers.Add(6554);
         numbers.Add(7878);
         numbers.Add(234);
         numbers.Add(87);
 
-        Console.WriteLine("Write a Number :");
-        int Number = Convert.ToInt32(Console.ReadLine());
-        int div = 5;
-        Console.WriteLine(Number);
-        Console.Read();
-        foreach(int a in numbers)
+        Console.WriteLine("Write a Number :");                                              // DISPLAY A MESSAGE TO REQUEST A NUMBER
+        int div;
+        
+        try { 
+            int numStr = Convert.ToInt32(Console.ReadLine());                               // READ THE USER INPUT AND CASTING TO A INTEGER
+            foreach(int a in numbers)
+            {
+                div = a / numStr;                                                           // DIVIDE ALL ELEMENTS BY THE USER NUMBER
+                Console.WriteLine( a.ToString() + " / " + numStr.ToString() + " is equals to "+ div.ToString());
+            }
+        }
+        catch (DivideByZeroException e)                                                     // EXCEPTION IF THE USER INSERTS A 0 VALUE
         {
-            //div = a / Number;
-            Console.WriteLine("{0} asasas {1} is equals to {3}", a, Number, div);
+            Console.WriteLine("You Cannot divide by 0");
+        }
+        catch(FormatException e)                                                            // EXCEPTION IF THE USER INSERTS A NON-INTEGER VALUE
+        {
+            Console.WriteLine("You must write an integer");
         }
 
-        Console.Read();
+        Console.WriteLine("\nThe program has emerged from the try/catch block");            // DISPLAYING A MESSAGE TO LET THE USER KNOWS THE PROGRAM HAS EMERGED FROM
+        Console.Read();                                                                 // TRY CATCH BLOCK
     }
 }
 
