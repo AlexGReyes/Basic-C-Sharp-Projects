2f9c60f9c3fd0d5a141083a33e5471c7ba77586d
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-06 10:44:42 -0800

    POLYMORPHISM SUBMISSION ASSIGNMENT

---
+++ polymorphismSubmission/polymorphismSubmission/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismSubmission
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()                          // IMPLEMENTED METHOD INHERIT FROM AN ABSTRACT CLASS
+        {
+            Console.WriteLine(fistName + " " + lastName);
+        }
+
+        public void Quit()                                      // IMPLEMENTED METHOD FROM AN INTEFACE
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("I Quit!!");
+        }
+    }
+}
---
+++ polymorphismSubmission/polymorphismSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable iq1 = new Employee();                // CREATION OF AN OBJECT OF TYPE IQUITTABLE
+            iq1.Quit();                                     // CALLING OF A METHOD
+            Console.Read();
+        }
+    }
+}
