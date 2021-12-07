17c7a33c7a13b8ec4c69b003347c2b3e6b246845
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-06 16:20:13 -0800

    PARSING ENUMS SUBMISSION ASSIGNMENT

---
+++ enumSubmission/enumSubmission/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please, write one day of the week");
+
+                day day = (day)Enum.Parse(typeof(day), Console.ReadLine().ToUpper());       // PARSING DATA AS A ENUM
+                                                                                            // 
+
+                Console.WriteLine(day);                                                     // DISPLAYING AS A STRING
+                Console.WriteLine(Convert.ToInt32(day));                                    // DISPLAYING ITS VALUE
+            }
+            catch (ArgumentException)                                                       // CATCH AN EXCEPTION IF THE INPUT DOESN'T MATCH WITH AN ENUM VALUE
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+
+            Console.Read();
+        }
+    }
+
+    public enum day                                                                         // A DAY OF THE WEEK ENUM
+    {
+        MONDAY, 
+        TUESDAY, 
+        WEDNESDAY, 
+        THURSDAY, 
+        FRIDAY, 
+        SATURDAY,
+        SUNDAY
+    }
+}
