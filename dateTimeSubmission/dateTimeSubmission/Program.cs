ea4082824205c1e357fb6f8bf6ad6d20758e19b3
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-08 12:54:57 -0800

    DATE/TIME SUBMISSION ASSIGNMENT

---
+++ dateTimeSubmission/dateTimeSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString());                                              // PRINTS THE CURRENT DATE AND TIME
+            Console.WriteLine("Please write a integer number");                             // REQUESTING FOR A INTEGER NUMBER
+
+            try
+            {   
+                uint num = UInt32.Parse(Console.ReadLine());                                // CASTING AS A 32-BIT INTEGER
+
+                DateTime timeAfterNow = now.AddHours(num);                                  // ADDING TO THE CURRENT DATE A SPECIFIC NUMBER OF HOURS
+                Console.WriteLine("The exact time will be at " + timeAfterNow.ToString());  // PRINTS THE NEW DATE AND TIME
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("You have to write a integer number");                    // A EXCEPTION IF THE USER DOES NOT WRITE A NUMBER
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ inputAssignment/inputAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace inputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a number");                             // REQUESTING FOR A NUMBER
+            string a = Console.ReadLine();
+            if (int.TryParse(a, out int value))                                     // IF THE STRING IS INTEGER VALUE
+            {
+                File.WriteAllText(@"C:\Users\Alex\log\text.txt", a);                // SAVE THE STRINF IN A FILE
+
+                Console.WriteLine(File.ReadAllText(@"C:\Users\Alex\log\text.txt")); // READING FROM FILE THE DATA SAVED
+            }
+            Console.Read();
+
+           
+        }
+    }
+}
