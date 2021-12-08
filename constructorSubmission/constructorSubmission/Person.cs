87f7e0da6c2a70c4069c642b01002e3aeccdcd76
Alejandro reyes <agreyesaldeco@gmail.com>
2021-12-08 15:39:11 -0800

    CONSTRUCTOR SUBMISSION ASSIGNMENT

---
+++ constructorSubmission/constructorSubmission/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorSubmission
+{
+    public class Person
+    {
+        public Person(int age) : this("Annonymous", "User", age)        // CHAINING A CONTRUCTOR
+        {
+
+        }
+
+        public Person(string firstName, string lastName, int id)       // MAIN CONSTRUCTOR
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+        }
+
+        public string FirstName { get; set; }                           // ADDING 3 PROPERTIES
+        public string LastName { get; set; }
+        public int ID { set; get; }
+    }
+}
---
+++ constructorSubmission/constructorSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace constructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SCHOOL = "The Tech Academy";                                                           // DECLARATION OF A CONSTANT VARIABLE
+
+            Person user01 = new Person("Alejandro", "Reyes", 35);                                               // CREATION OF AN INSTANCE OF PERSON
+
+            Console.WriteLine("{0} {1}({2}) Welcome to {3}\n\n\n", user01.FirstName, user01.LastName, user01.ID, SCHOOL);// DISPLAYING DATA FROM PERSON AND THE SCHOOL
+
+
+            var user02 = new Person(30);                                                                        // DELARATION OF AN INSTANCE OF PERSON USING KEYWORD VAR
+            Console.WriteLine("{0} {1}({2}) Welcome to {3}\n\n\n", user02.FirstName, user02.LastName, user02.ID, SCHOOL);
+
+            Console.Read();
+
+
+        }
+    }
+}
